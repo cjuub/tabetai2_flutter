1232315d5d7959c9421d6c0d43cda67d86b7868b
cjuub <cjuuub@gmail.com>
2020-09-08 22:21:15 +0200

    Add ability to select and view recipe

--- lib/recipe_list.dart
+++ lib/recipe_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tabetai2_flutter/recipe_view.dart';
 import 'package:tabetai2_flutter/wamp_session.dart';
 
 class RecipeList extends StatefulWidget {
@@ -26,11 +27,25 @@ class _RecipeListState extends State<RecipeList> {
     for (dynamic recipe in widget._recipeData) {
       _recipeNames.add(recipe[1]);
     }
-
+    
     if (_recipeNames.isEmpty) {
       return Text('No recipes to show');
     }
 
-    return Text('$_recipeNames');
+    return ListView.builder(
+        shrinkWrap: true,
+        padding: const EdgeInsets.all(8),
+        itemCount: _recipeNames.length,
+
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+//            onTap: () => print(widget._recipeData[index]),
+            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => RecipeView(widget._recipeData[index]))),
+            child: Container(
+              height: 50,
+              child: Center(child: Text('${_recipeNames[index]}')),
+            ),
+          );
+        }).build(context);
   }
 }
